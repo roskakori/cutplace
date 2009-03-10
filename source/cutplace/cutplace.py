24011345f1bbb4d43192252cca2c7a946bc10908
Thomas Aglassinger <roskakori@users.sourceforge.net>
2009-03-10 00:13:36 +0000

    - Added reading of data file but no actual validation yet. - Added mapping of some values of DataFormat to Dialect. It is still incomplete though. - Changed messages quoting data values to use repr() instead of str() in order to display special characters in a reasonable way.

--- source/cutplace/cutplace.py
+++ source/cutplace/cutplace.py
@@ -2,6 +2,7 @@
 import csv
 import getopt
 import logging
+import parsers
 import re
 import sys
 import tools
@@ -17,21 +18,21 @@ class DataFormat(object):
     """Properties from "Data format" section of ICD."""
     def __init__(self):
         self.KEY_ALLOWED = "allowed"
-        self.KEY_ENCODING  = "encoding"
+        self.KEY_ENCODING = "encoding"
         self.KEY_DECIMAL_SEPARATOR = "decimalSeparator"
         self.KEY_ITEM_SEPARATOR = "itemSeparator"
         self.KEY_LINE_SEPARATOR = "lineSeparator"
         self.KEY_DECIMAL_SEPARATOR = "decimalSeparator"
         self.KEY_THOUSANDS_SEPARATOR = "thousandsSeparator"
-        self.KEY_TYPE = "format"
+        self.KEY_FORMAT = "format"
         self.VALID_KEYS = [
-                           self.KEY_ALLOWED, 
-                           self.KEY_DECIMAL_SEPARATOR, 
-                           self.KEY_ENCODING, 
-                           self.KEY_ITEM_SEPARATOR, 
-                           self.KEY_LINE_SEPARATOR, 
-                           self.KEY_THOUSANDS_SEPARATOR, 
-                           self.KEY_TYPE
+                           self.KEY_ALLOWED,
+                           self.KEY_DECIMAL_SEPARATOR,
+                           self.KEY_ENCODING,
+                           self.KEY_ITEM_SEPARATOR,
+                           self.KEY_LINE_SEPARATOR,
+                           self.KEY_THOUSANDS_SEPARATOR,
+                           self.KEY_FORMAT
                            ]
         self.TYPE_CSV = "csv"
         self.VALID_TYPES = [
@@ -43,15 +44,18 @@ class DataFormat(object):
     def reset(self):
         self.allowed = None
         self.encoding = None
-        self.separator = None
-        self.type = None
+        self.decimaleSeparator = None
+        self.thousandsSeparator = None
+        self.format = None
+        self.lineSeparator = parsers.AUTO
+        self.itemSeparator = parsers.AUTO
 
     def set(self, key, value):
         """Set property key to value."""
         assert key is not None
         actualKey = tools.camelized(key, True)
         if not actualKey in self.VALID_KEYS:
-            raise LookupError("data format key is \"%s\" but must be one of: %s" %(actualKey, str(self.VALID_KEYS)))
+            raise LookupError("data format key is %s but must be one of: %s" % (repr(actualKey), str(self.VALID_KEYS)))
         if value is None:
             actualValue = None
         else:
@@ -59,17 +63,29 @@ class DataFormat(object):
             if actualValue == "":
                 actualValue = None
             if actualValue is not None:
-                if actualKey == self.KEY_TYPE:
+                if actualKey == self.KEY_FORMAT:
                     if not actualValue.lower() in self.VALID_TYPES:
-                        raise LookupError("data format type is \"%s\" but must be one of: %s" %(str(value), str(self.VALID_TYPES)))
-        self.log.debug("set %s to \"%s\"" %(actualKey, str(actualValue)))
+                        raise LookupError("data format type is \"%s\" but must be one of: %s" % (str(value), str(self.VALID_TYPES)))
+        self.log.debug("set %s to \"%s\"" % (actualKey, str(actualValue)))
         setattr(self, actualKey, value)
+        
+    def lineSeparatorForDialect(self):
+        lineSeparator = self.lineSeparator.lower()
+        if lineSeparator == "cr":
+            result = parsers.CR
+        elif lineSeparator == "crlf":
+            result = parsers.CRLF
+        elif lineSeparator == "lf":
+            result = parsers.LF
+        else:
+            raise ValueError("lineSeparator=" + repr(lineSeparator))
+        return result
 
 class FieldValueError(ValueError):
     """Error raised when AbstractFieldFormat.validate detects an error."""
 
 class AbstractFieldFormat(object):
-    """Description of a field."""
+    """Format description of a field in a data file to validate."""
     def __init__(self, fieldName, rule, isAllowedToBeEmpty):
         assert fieldName is not None
         assert fieldName, "fieldName must not be empty"
@@ -91,11 +107,11 @@ class ChoiceFieldFormat(AbstractFieldFormat):
         for choice in rule.lower().split(","):
             self.choices.append(choice.trim())
         if not choices:
-            raise ValueError("at least one choice must be specified for a \"Choice\" field")
+            raise ValueError("at least one choice must be specified for a %s field" % (repr("choice")))
     
     def validate(self, value):
         if value.lower() not in sefl.choices:
-            raise FieldValueError("value is \"%s\" but must be one of: %s" %(value, str(choices)))
+            raise FieldValueError("value is %s but must be one of: %s" % (repr(value), str(choices)))
     
 class IntegerFieldFormat(AbstractFieldFormat):
     def __init__(self, fieldName, rule, isAllowedToBeEmpty):
@@ -106,7 +122,7 @@ class IntegerFieldFormat(AbstractFieldFormat):
         try:
             longValue = long(value)
         except ValueError:
-            raise FieldValueError("value must be an integer number: \"%s\"" %(str(value)))
+            raise FieldValueError("value must be an integer number: %s" % (repr(value)))
         if self.rule:
             # TODO: Validate range
             pass 
@@ -126,7 +142,7 @@ class RegExFieldFormat(AbstractFieldFormat):
 
     def validate(self, value):
         if not self.regex.match(value):
-            raise FieldValueError("value \"%s\" must match regular expression: \"%s\"" %(str(value), str(self.rule)))
+            raise FieldValueError("value %s must match regular expression: %s" % (repr(value), repr(self.rule)))
 
 class TextFieldFormat(AbstractFieldFormat):
     def __init__(self, fieldName, rule, isAllowedToBeEmpty):
@@ -140,7 +156,7 @@ class TextFieldFormat(AbstractFieldFormat):
 class InterfaceDescription(object):
     """Model the data driven parts of an Interface Control Document (ICD)."""
     def __init__(self):
-        self.EMPTY_INDICATOR  = "x"
+        self.EMPTY_INDICATOR = "x"
         self.ID_CONSTRAINT = "c"
         self.ID_DATA_FORMAT = "d"
         self.ID_FIELD_RULE = "f"
@@ -157,18 +173,18 @@ class InterfaceDescription(object):
         if lastDotIndex >= 0:
             # FIXME: Detect and report errors for  cases ".class" and "module.".
             moduleName = fieldType[0:lastDotIndex]
-            className = fieldType[lastDotIndex+1:]
+            className = fieldType[lastDotIndex + 1:]
             module = __import__(moduleName)
         else:
             moduleName = __name__
             className = fieldType
             module = sys.modules[__name__]
         className += "FieldFormat"
-        log.debug("create from "  + str(moduleName) + " class " + className)
+        log.debug("create from " + str(moduleName) + " class " + className)
         try:
             result = getattr(module, className)
         except AttributeError:
-            raise LookupError("cannot find field format: %s" %(str(fieldType)))
+            raise LookupError("cannot find field format: %s" % (str(fieldType)))
         return result
 
     def addDataFormat(self, items):
@@ -182,7 +198,7 @@ class InterfaceDescription(object):
                 value = None
             self.dataFormat.set(key, value)
         else:
-            raise IndexError("data format line (marked with \"" + self.ID_DATA_FORMAT + "\") must contain at least 2 columns")
+            raise IndexError("data format line (marked with %s) must contain at least 2 columns" % (repr(self.ID_DATA_FORMAT)))
 
     def addFieldFormat(self, items):
         assert items is not None
@@ -201,14 +217,14 @@ class InterfaceDescription(object):
                     if fieldIsAllowedToBeEmptyText == self.EMPTY_INDICATOR:
                         fieldIsAllowedToBeEmpty = True
                     elif fieldIsAllowedToBeEmptyText:
-                        raise ValueError("Mark for empty field is \"%s\" but must be \"%s\"" %(fieldIsAllowedToBeEmptyText, self.EMPTY_INDICATOR))
+                        raise ValueError("Mark for empty field is %s but must be %s" % (repr(fieldIsAllowedToBeEmptyText), repr(self.EMPTY_INDICATOR)))
             else:
                 fieldRule = None
             fieldClass = self._createFieldFormatClass(fieldType);
-            self.log.debug("create field: %s(%s, %s, %s)" %(str(fieldClass), str(fieldName), str(fieldType), str(fieldRule)))
+            self.log.debug("create field: %s(%s, %s, %s)" % (str(fieldClass), str(fieldName), str(fieldType), str(fieldRule)))
             fieldFormat = fieldClass.__new__(fieldClass, fieldName, fieldRule, True)
         else:
-            raise IndexError("field format line (marked with \"" + self.ID_FIELD_RULE + "\") must contain at least 3 columns")
+            raise IndexError("field format line (marked with %s) must contain at least 3 columns" % (repr(self.ID_FIELD_RULE)))
         
     def read(self, icdFilePath):
         icdFile = open(icdFilePath, "rb")
@@ -218,7 +234,7 @@ class InterfaceDescription(object):
             reader = csv.reader(icdFile, dialect)
             for row in reader:
                 lineNumber = reader.line_num
-                self.log.debug("parse icd line%5d: %s" %(lineNumber, str(row)))
+                self.log.debug("parse icd line%5d: %s" % (lineNumber, str(row)))
                 if len(row) >= 1:
                     rowId = str(row[0]).lower() 
                     if rowId == self.ID_CONSTRAINT:
@@ -228,20 +244,25 @@ class InterfaceDescription(object):
                     elif rowId == self.ID_FIELD_RULE:
                         self.addFieldFormat(row[1:])
                     elif rowId:
-                        raise ValueError("first row in line %d is \"%s\" but must be empty or one of: %s" %(lineNumber, str(row[0]), str(self.VALID_IDS)))
+                        raise ValueError("first row in line %d is %s but must be empty or one of: %s" % (lineNumber, repr(row[0]), str(self.VALID_IDS)))
         finally:
             icdFile.close()
             
     def validate(self, dataFileToValidatePath):
         """Validate that all lines and items in dataFileToValidatePath conform to this interface."""
         assert dataFileToValidatePath is not None
-        self.log.info("validate \"%s\"" %(dataFileToValidatePath))
-        if self.dataFormat.type == DataFormat.TYPE_CSV:
-            dialect = csv.Dialect
+        self.log.info("validate \"%s\"" % (dataFileToValidatePath))
+        
+        # TODO: Clean up lower(), it should be called at a more appropriate place such as a property setter.
+        if self.dataFormat.format.lower() == self.dataFormat.TYPE_CSV:
             dataFile = open(dataFileToValidatePath)
             try:
-                for items in csv.reader(dataFile, dialect):
-                    pass
+                dialect = parsers.DelimitedDialect()
+                dialect.lineDelimiter = self.dataFormat.lineSeparatorForDialect()
+                dialect.itemDelimiter = self.dataFormat.itemSeparator
+                reader = parsers.delimitedReader(dataFile, dialect)
+                for row in reader:
+                    print row
             finally:
                 dataFile.close()
         else:
@@ -359,4 +380,4 @@ if __name__ == '__main__':
         cutPlace._printHeader()
     else:
         for path in cutPlace.dataToValidatePaths:
-            log.info("validate %s according to %s" % (path, cutPlace.interfaceSpecificationPath))
+            cutPlace.icd.validate(path)
