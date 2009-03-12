a3ca9f8eef143f79ce74aaedfdb7ca650f19e557
Thomas Aglassinger <roskakori@users.sourceforge.net>
2009-03-12 18:13:49 +0000

    Added test cases for field formats.

---
+++ source/cutplace/test_fields.py
@@ -0,0 +1,76 @@
+"""Tests  for field formats."""
+import fields
+import logging#
+import unittest
+
+class IntegerFieldFormatTest(unittest.TestCase):
+    """Tests  for IntegerFieldFormat."""
+    def testWithinRange(self):
+        format = fields.IntegerFieldFormat("x", "1...10", False)
+        format.validate("1")
+        format.validate("7")
+        format.validate("10")
+
+    def testTooSmall(self):
+        format = fields.IntegerFieldFormat("x", "1...10", False)
+        self.assertRaises(fields.FieldValueError, format.validate, "0")
+
+    def testTooBig(self):
+        format = fields.IntegerFieldFormat("x", "1...10", False)
+        self.assertRaises(fields.FieldValueError, format.validate, "11")
+        
+    def testNoNumber(self):
+        format = fields.IntegerFieldFormat("x", "1...10", False)
+        self.assertRaises(fields.FieldValueError, format.validate, "abc")
+        
+class RegExFieldFormatTest(unittest.TestCase):
+    """Tests  for RegExFieldFormat."""
+    def testMatch(self):
+        format = fields.RegExFieldFormat("x", r"a.*", False)
+        format.validate("abc")
+
+    def testNoMatch(self):
+        format = fields.RegExFieldFormat("x", r"a.*", False)
+        self.assertRaises(fields.FieldValueError, format.validate, "xyz")
+    
+    def testBrokenRegEx(self):
+        try:
+            format = fields.RegExFieldFormat("x", "*", False)
+            self.fail("broken pattern must raise error")
+        except:
+            # Ignore error caused by broken pattern. It would be better to use assertFails but
+            # the interface to re.compile doesn't document a specific exception to be raised in
+            # such a case.
+            pass
+
+class ChoiceFieldFormatTest(unittest.TestCase):
+    """Tests  for RegExFieldFormat."""
+    def testMatchingChoice(self):
+        format = fields.ChoiceFieldFormat("color", "red,grEEn, blue ", False)
+        format.validate("red")
+        # Value without blanks around it.
+        format.validate("grEEn")
+        # Value with blanks around it.
+        format.validate("blue")
+        # Disregard upper/lower case.
+        format.validate("gReen")
+        
+    def testImproperChoice(self):
+        format = fields.ChoiceFieldFormat("color", "red,green, blue ", False)
+        self.assertRaises(fields.FieldValueError, format.validate, "tree")
+        
+    def testMatchingOnlyChoice(self):
+        format = fields.ChoiceFieldFormat("color", "red", False)
+        format.validate("red")
+
+    def testEmptyChoice(self):
+        # FIXME: Should cause ValueError
+        format = fields.ChoiceFieldFormat("color", " ", False)
+        format = fields.ChoiceFieldFormat("color", "red, ", False)
+        
+
+
+if __name__ == '__main__':
+    logging.basicConfig()
+    logging.getLogger("cutplace").setLevel(logging.INFO)
+    unittest.main()
