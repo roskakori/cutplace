c67890796a69932a93ea3a5bcace8032c74c19aa
Thomas Aglassinger <roskakori@users.sourceforge.net>
2009-04-05 19:01:39 +0000

    - Refactored OdsContentHandler to AbstractOdsContentHandler and moved CSV functionality to separate OdsToCsvContentHandler. This allows to easily support other output formats. - Cleaned up formatting.

--- source/cutplace/ods.py
+++ source/cutplace/ods.py
@@ -6,14 +6,11 @@ import sys
 import xml.sax
 import zipfile
 
-class OdsContentHandler(xml.sax.ContentHandler):
+class AbstractOdsContentHandler(xml.sax.ContentHandler):
     """Sax ContentHandler for content.xml in ODS."""
     # FIXME: Read only first table.
-    def __init__(self, csvWriter):
-        assert csvWriter is not None
-        
+    def __init__(self):
         xml.sax.ContentHandler.__init__(self)
-        self.csvWriter = csvWriter
         self._log = logging.getLogger("cutplace.ods")
         
     def startDocument(self):
@@ -29,16 +26,16 @@ class OdsContentHandler(xml.sax.ContentHandler):
                 self.numberColumnsRepeated = long(attributes.getValue("table:number-columns-repeated"))
             except KeyError:
                 self.numberColumnsRepeated = 1
-            self._log.debug("%s<%s> (%d) %r" %(" " * 2 * self.indent, name, self.numberColumnsRepeated, attributes.items()))
+            self._log.debug("%s<%s> (%d) %r" % (" " * 2 * self.indent, name, self.numberColumnsRepeated, attributes.items()))
             self.insideCell = True
             self.cellText = ""
         elif name == "table:table-row":
-            self._log.debug("%s<%s>" %(" " * 2 * self.indent, name))
+            self._log.debug("%s<%s>" % (" " * 2 * self.indent, name))
             self.row = []
 
     def characters(self, text):
         if self.insideCell:
-            self._log.debug("%s%r" %(" " * 2 * (self.indent + 1), text))
+            self._log.debug("%s%r" % (" " * 2 * (self.indent + 1), text))
             self.cellText += text
 
     def endElement(self, name):
@@ -51,16 +48,32 @@ class OdsContentHandler(xml.sax.ContentHandler):
             self.cellText = None
         if name == "table:table-row":
             assert self.row is not None
-            self.csvWriter.writerow(self.row)
-            self.row = None 
+            self.rowCompleted()
+            self.row = None
+            
+    def rowCompleted(self):
+        """
+        Actions to be performed once `self.row` is complete and can be processed.
+        """
+        raise NotImplementedError("rowCompleted must be implemented")
         
+class OdsToCsvContentHandler(AbstractOdsContentHandler):
+    def __init__(self, csvWriter):
+        assert csvWriter is not None
+
+        AbstractOdsContentHandler.__init__(self)
+        self.csvWriter = csvWriter
+
+    def rowCompleted(self):
+        self.csvWriter.writerow(self.row)
+
 def toCsv(odsFilePath, csvTargetPath, dialect="excel"):
     assert odsFilePath is not None
     
-    logging.getLogger("cutplace.ods").info("convert %r to %r" %(odsFilePath, csvTargetPath))
+    logging.getLogger("cutplace.ods").info("convert %r to %r" % (odsFilePath, csvTargetPath))
     zipArchive = zipfile.ZipFile(odsFilePath, "r")
     try:
-        # TODO: Consider switching to 2.6 and use ZipFile.open().
+        # TODO: Consider switching to 2.6 and use ZipFile.open(). This would need less memory.
         xmlData = zipArchive.read("content.xml")
     finally:
         zipArchive.close()
@@ -68,7 +81,7 @@ def toCsv(odsFilePath, csvTargetPath, dialect="excel"):
     csvTargetFile = open(csvTargetPath, "w")
     try:
         csvWriter = csv.writer(csvTargetFile, dialect)
-        xml.sax.parseString(xmlData, OdsContentHandler(csvWriter))
+        xml.sax.parseString(xmlData, OdsToCsvContentHandler(csvWriter))
     finally:
         csvTargetFile.close()
  
@@ -81,4 +94,4 @@ if __name__ == '__main__':
         except Exception, error:
             logging.getLogger("cutplace.ods").error("cannot convert ods to csv: %s" % str(error), exc_info=1)
     else:
-        sys.stderr.write("Usage: %s {source.ods} {target.csv}%s" %(sys.argv[0], os.linesep))
+        sys.stderr.write("Usage: %s {source.ods} {target.csv}%s" % (sys.argv[0], os.linesep))
