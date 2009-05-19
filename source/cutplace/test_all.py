dec29e8a25c4096478bf33120c1ae5e26e388920
Thomas Aglassinger <roskakori@users.sourceforge.net>
2009-05-19 19:51:30 +0000

    Added doctest tests.

--- source/cutplace/test_all.py
+++ source/cutplace/test_all.py
@@ -13,39 +13,58 @@ import test_range
 import test_tools
 import test_web
 import unittest
+import checks
+import cutplace
+import data
+import doctest
+import fields
+import interface
+import ods
+import parsers
+import range
+import server
+import tools
+import version
 
 def main():
     loader = unittest.TestLoader()
     testCount = 0
     errorCount = 0
     failureCount = 0
-    
+
+    allTestSuite = unittest.TestSuite()
+    # TODO: Automatically discover doctest cases.
+    for module in checks, cutplace, data, fields, interface, ods, parsers, range, server, tools, version:
+        allTestSuite.addTest(doctest.DocTestSuite(module))    
+
     # TODO: Automatically discover test cases.
-    for testCaseClass in [
-                        test_checks.DistinctCountCheckTest,
-                        test_checks.IsUniqueCheckTest,
-                        test_cutplace.CutplaceTest,
-                        test_data.DataFormatTest,
-                        test_interface.InterfaceControlDocumentTest,
-                        test_fields.AbstractFieldFormatTest,
-                        test_fields.ChoiceFieldFormatTest,
-                        test_fields.DateTimeFieldFormatTest,
-                        test_fields.IntegerFieldFormatTest,
-                        test_fields.PatternFieldFormatTest,
-                        test_fields.RegExFieldFormatTest,
-                        test_parsers.DelimitedParserTest,
-                        test_parsers.ExcelParserTest,
-                        test_parsers.FixedParserTest,
-                        test_range.RangeTest,
-                        test_tools.ToolsTest
-                        # FIXME: Stop server and add: test_web.WebTest
-                        ]:
-        suite = loader.loadTestsFromTestCase(testCaseClass)
-        testResults = unittest.TextTestRunner(verbosity=2).run(suite)
-        testCount += testResults.testsRun
-        failureCount += len(testResults.failures)
-        errorCount += len(testResults.errors)
-    print "test_all: Ran %d tests with %d failures and %d errors" % (testCount, failureCount, errorCount)
+    allTests = [
+            test_checks.DistinctCountCheckTest,
+            test_checks.IsUniqueCheckTest,
+            test_cutplace.CutplaceTest,
+            test_data.DataFormatTest,
+            test_interface.InterfaceControlDocumentTest,
+            test_fields.AbstractFieldFormatTest,
+            test_fields.ChoiceFieldFormatTest,
+            test_fields.DateTimeFieldFormatTest,
+            test_fields.IntegerFieldFormatTest,
+            test_fields.PatternFieldFormatTest,
+            test_fields.RegExFieldFormatTest,
+            test_parsers.DelimitedParserTest,
+            test_parsers.ExcelParserTest,
+            test_parsers.FixedParserTest,
+            test_range.RangeTest,
+            test_tools.ToolsTest
+            # FIXME: Stop server and add: test_web.WebTest
+            ]
+    for testCaseClass in allTests:
+        allTestSuite.addTest(loader.loadTestsFromTestCase(testCaseClass))
+
+    testResults = unittest.TextTestRunner(verbosity=2).run(allTestSuite)
+    testCount += testResults.testsRun
+    failureCount += len(testResults.failures)
+    errorCount += len(testResults.errors)
+    print "test_all: ran %d tests with %d failures and %d errors" % (testCount, failureCount, errorCount)
     if (errorCount + failureCount) > 0:
         sys.exit(1)
     
