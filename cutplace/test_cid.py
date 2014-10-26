572c333dfe16ab19093849a49f85f18531053c3d
Patrick Heuberger <d10041@student.htlwrn.ac.at>
2014-10-26 14:34:42 +0100

    #63 Add Excel-CID reader for data format
     * implemented unittests for the new reader function
     * moved excel_rows from cid.py to _tools.py

--- cutplace/test_cid.py
+++ cutplace/test_cid.py
@@ -20,6 +20,9 @@ Tests for `cid` module
 
 from cutplace import cid
 from cutplace import dev_test
+from cutplace import _tools
+from cutplace import errors
+import os
 
 import logging
 import unittest
@@ -31,12 +34,44 @@ class Cidtest(unittest.TestCase):
     """
     _TEST_ENCODING = "cp1252"
 
-    def test_can_read_excel_and_create_dataformat(self):
+    def test_can_read_excel_and_create_dataformat_delimited(self):
         cid_reader = cid.Cid()
-        cid_reader.read(dev_test.getTestIcdPath("icd_customers.xls"))
+        source_path = dev_test.getTestIcdPath("icd_customers.xls")
+        print(source_path)
+        cid_reader.read(source_path, _tools.excel_rows(source_path))
 
         self.assertEqual(cid_reader._data_format.format, "delimited")
         self.assertEqual(cid_reader._data_format.header, 1)
 
+    def test_fails_on_empty_data_format_property_name(self):
+        cid_reader = cid.Cid()
+        self.assertRaises(errors.DataFormatSyntaxError, cid_reader.read, 'inline', [
+            ['d', 'format', 'delimited'],
+            ['d', '', ''],
+        ])
 
+    def test_fails_on_missing_data_format_value_name(self):
+        cid_reader = cid.Cid()
+        self.assertRaises(errors.DataFormatSyntaxError, cid_reader.read,'inline', [
+            ['d', 'format', 'delimited'],
+            ['d', 'header'],
+        ])
+
+    def test_fails_on_missing_data_format_property_name(self):
+        cid_reader = cid.Cid()
+        self.assertRaises(errors.DataFormatSyntaxError, cid_reader.read,'inline', [
+            ['d', 'format', 'delimited'],
+            ['d'],
+        ])
+
+    def test_fails_on_invalid_row_typ(self):
+        cid_reader = cid.Cid()
+        self.assertRaises(errors.DataFormatSyntaxError, cid_reader.read,'inline',[['x']])
+
+    def test_can_skip_empty_rows(self):
+        cid_reader = cid.Cid()
+        cid_reader.read('inline',[
+            [],
+            ['d','format','delimited']])
+        self.assertEqual(cid_reader._data_format.format, "delimited")
 
