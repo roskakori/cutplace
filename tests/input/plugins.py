d1556f50324ef911724c77ce0dfeeafa2429b492
Thomas Aglassinger <roskakori@users.sourceforge.net>
2014-12-02 00:55:48 +0100

    * #56: Add support for Python 3. * #61: Add __future__ imports for Python 2. * #66: Cleanup PEP8 issues.

--- tests/input/plugins.py
+++ tests/input/plugins.py
@@ -1,20 +1,22 @@
 """
 Plugins to be used for tests.
 """
-try:
-    # HACK: Import used when launched from shell or eclipse.
-    import fields
-except ImportError:
-    # HACK: Import used when launched from ant.
-    from cutplace import fields
+from __future__ import absolute_import
+from __future__ import divison
+from __future__ import print_function
+from __future__ import unicode_literals
+
+from cutplace import fields
+
 
 class CapitalizedTextFieldFormat(fields.AbstractFieldFormat):
     """
-    Field format to validate for ``value`` to start in upper case. This is used test plugin support.
+    Field format to validate for ``value`` to start in upper case. This is used to test plugin support.
     """
-    def validatedValue(self, value):
+    def validated_value(self, value):
         if value:
-            firstChar = value[0]
-            if not firstChar.isupper():
-                raise fields.FieldValueError("first character %r must be changed to upper case: %r" % (firstChar, value))
+            first_char = value[0]
+            if not first_char.isupper():
+                raise fields.FieldValueError("first character %r must be changed to upper case: %r"
+                    % (first_char, value))
         return value
