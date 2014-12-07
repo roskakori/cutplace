4677f296934dc50278d74875324146aca24baea3
Thomas Aglassinger <roskakori@users.sourceforge.net>
2014-12-07 08:12:38 +0100

    #76: Add support for ODS files.

--- tests/input/plugins.py
+++ tests/input/plugins.py
@@ -2,7 +2,7 @@
 Plugins to be used for tests.
 """
 from __future__ import absolute_import
-from __future__ import divison
+from __future__ import division
 from __future__ import print_function
 from __future__ import unicode_literals
 
