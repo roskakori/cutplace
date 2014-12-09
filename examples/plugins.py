82ff20d78a9fd76e2b2d5984d335bbd6accae68e
Thomas Aglassinger <roskakori@users.sourceforge.net>
2014-12-09 17:43:39 +0100

    #58: Move exceptions to errors module.
    Fixed broken example.

--- examples/plugins.py
+++ examples/plugins.py
@@ -2,6 +2,7 @@
 Example plugins for cutplace.
 """
 from cutplace import checks
+from cutplace import errors
 from cutplace import fields
 from cutplace import ranges
 
@@ -9,9 +10,9 @@ class ColorFieldFormat(fields.AbstractFieldFormat):
     """Field format representing colors as their names."""
     def __init__(self, fieldName, isAllowedToBeEmpty, length, rule, dataFormat):
         super(ColorFieldFormat, self).__init__(fieldName, isAllowedToBeEmpty, length, rule, dataFormat,
-                emptyValue=(0.0, 0.0, 0.0)) # Use black as "empty" color.
+                empty_value=(0.0, 0.0, 0.0))  # Use black as "empty" color.
 
-    def validatedValue(self, colorName):
+    def validated_value(self, colorName):
         assert colorName
         if colorName == "red":
             result = (1.0, 0.0, 0.0)
@@ -20,7 +21,7 @@ class ColorFieldFormat(fields.AbstractFieldFormat):
         elif colorName == "blue":
             result = (0.0, 1.0, 0.0)
         else:
-            raise fields.FieldValueError("color name is %r but must be one of: red, green, blue" % colorName)
+            raise errors.FieldValueError("color name is %r but must be one of: red, green, blue" % colorName)
         return result
 
 class FullNameLengthIsInRangeCheck(checks.AbstractCheck):
@@ -30,11 +31,11 @@ class FullNameLengthIsInRangeCheck(checks.AbstractCheck):
         self._fullNameRange = ranges.Range(rule)
         self.reset()
 
-    def checkRow(self, rowMap, location):
+    def check_row(self, rowMap, location):
         fullName = rowMap["last_name"] + ", " + rowMap["first_name"]
         fullNameLength = len(fullName)
         try:
             self._fullNameRange.validate("full name", fullNameLength)
-        except ranges.RangeValueError as error:
-            raise CheckError("full name length is %d but must be in range %s: %r" \
+        except errors.RangeValueError as error:
+            raise errors.CheckError("full name length is %d but must be in range %s: %r" \
                 % (fullNameLength, self._fullNameRange, fullName))
