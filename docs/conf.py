604f410d1ab99ea88b349665cc6e42200612a0fe
Thomas Aglassinger <roskakori@users.sourceforge.net>
2015-01-11 10:20:35 +0100

    #74: Cleanup project structure and build process.
    Fixed web site logo.

--- docs/conf.py
+++ docs/conf.py
@@ -135,7 +135,7 @@ html_logo = "_static/cutplace_logo_64x64.png"
 # The name of an image file (within the static path) to use as favicon of the
 # docs.  This file should be a Windows icon file (.ico) being 16x16 or 32x32
 # pixels large.
-html_favicon = "cutplace_logo_32x32.ico"
+html_favicon = "_static/cutplace_logo_32x32.ico"
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
