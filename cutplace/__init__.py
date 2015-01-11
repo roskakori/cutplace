22296c0bbf935b6adeee5bebc1bad45608d9c520
Thomas Aglassinger <roskakori@users.sourceforge.net>
2015-01-11 11:36:45 +0100

    #74: Cleanup project structure and build process.
    Added shortcuts for the most common classes and functions.

--- cutplace/__init__.py
+++ cutplace/__init__.py
@@ -4,21 +4,32 @@ and PRN files conform to an interface definition (CID).
 
 Additionally to the command line tool the functionality of cutplace is also
 accessible through a Python API.
-
-Module overview
----------------
-
-* :py:mod:`~cutplace.checks` - standard checks for rows and whole data sets
-* :py:mod:`~cutplace.data` - data formats to describe the basic structure of a data set
-* :py:mod:`~cutplace.errors` - all errors raised by cutplace
-* :py:mod:`~cutplace.fields` - standard field formats and an abstract field format tha can easily be extended
-* :py:mod:`~cutplace.interface` - everything need to describe a data set
-* :py:mod:`~cutplace.iotools` - basic input and output of tabular data without low level validation
-* :py:mod:`~cutplace.ranges` - basic classes to describe ranges of values
-* :py:mod:`~cutplace.validator` - high level reading and validation tabular data
 """
+from cutplace.data import DataFormat, FORMAT_DELIMITED, FORMAT_EXCEL, FORMAT_FIXED, FORMAT_ODS
+from cutplace.errors import \
+    CheckError, CutplaceError, DataError, DataFormatError, FieldValueError, InterfaceError, Location, RangeValueError
+from cutplace.interface import Cid
 from cutplace._version import get_versions
 
 #: Package version information.
 __version__ = get_versions()['version']
 del get_versions
+
+#: Public classes and functions.
+__all__ = [
+    'Cid',
+    'CheckError',
+    'CutplaceError',
+    'DataError',
+    'DataFormat',
+    'DataFormatError',
+    'FORMAT_DELIMITED',
+    'FORMAT_EXCEL',
+    'FORMAT_FIXED',
+    'FORMAT_ODS',
+    'FieldValueError',
+    'InterfaceError',
+    'Location',
+    'RangeValueError',
+    '__version__'
+]
