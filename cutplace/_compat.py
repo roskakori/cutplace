7edebb0d3a693c2724603a6a2f29e2016eec4d78
Thomas Aglassinger <roskakori@users.sourceforge.net>
2015-01-18 16:27:17 +0100

    Added copyright comment.

--- cutplace/_compat.py
+++ cutplace/_compat.py
@@ -5,6 +5,21 @@ Part of the code found here is derived from
 https://pypi.python.org/pypi/future and
 https://docs.python.org/2/library/csv.html#examples.
 """
+# Copyright (C) 2009-2015 Thomas Aglassinger
+#
+# This program is free software: you can redistribute it and/or modify it
+# under the terms of the GNU Lesser General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or (at your
+# option) any later version.
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+# FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public License
+# for more details.
+#
+# You should have received a copy of the GNU Lesser General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 # TODO: Probably we can eventually replace ``six`` by ``future`` from
 #  and remove `_compat` all together.
 import csv
